7b9d94f362f60f81a6db9529be793e0d4ba2194a
Susan Holland <segh@fastmail.com>
2021-05-06 19:26:15 -0400

    Change parameter from t to s

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 const API = {
     getMovies: async function(query) {
-        const response = await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}&t=${query}&type=movie`);
+        const response = await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}&s=${query}&type=movie`);
         const movies = await response.json();
         return movies;
     }
