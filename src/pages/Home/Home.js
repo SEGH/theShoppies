d4767c2a3d7be35d5950da3260b77e435ca760cb
Susan Holland <segh@fastmail.com>
2021-05-06 16:36:21 -0400

    Add handleInputChange function to setSearchValue state, pass to SearchBar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,19 @@
+import React, { useState } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
 
 export default function Home() {
-
+    const [searchValue, setSearchValue] = useState("");
     
+    const handleInputChange = event => {
+        const { value } = event.target;
+        console.log(value)
+        setSearchValue(value);
+    }
+
     return (
         <main>
             <h1>The Shoppies</h1>
-            <SearchBar />
+            <SearchBar handleInputChange={handleInputChange} />
         </main>
     );
 }
