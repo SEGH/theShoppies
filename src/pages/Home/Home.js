dd8ed63a6005e1a768fb49dfca130d93d773ebfb
Susan Holland <segh@fastmail.com>
2021-05-06 16:30:29 -0400

    Import SearchBar component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,12 @@
+import SearchBar from '../../components/SearchBar/SearchBar';
+
 export default function Home() {
+
+    
     return (
         <main>
             <h1>The Shoppies</h1>
+            <SearchBar />
         </main>
     );
 }
