c5911545528707088fce197f0370ae016b8c0e36
Susan Holland <segh@fastmail.com>
2021-05-06 16:48:11 -0400

    Add handleSearch function to log and clear searchValue if exists, pass to SearchBar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,17 +3,26 @@ import SearchBar from '../../components/SearchBar/SearchBar';
 
 export default function Home() {
     const [searchValue, setSearchValue] = useState("");
-    
+
     const handleInputChange = event => {
         const { value } = event.target;
         console.log(value)
         setSearchValue(value);
     }
 
+    const handleSearch = event => {
+        event.preventDefault();
+
+        if (searchValue) {
+            console.log(`Searching ${searchValue}...`)
+            setSearchValue("");
+        }
+    }
+
     return (
         <main>
             <h1>The Shoppies</h1>
-            <SearchBar handleInputChange={handleInputChange} />
+            <SearchBar handleInputChange={handleInputChange} handleSearch={handleSearch} searchValue={searchValue} />
         </main>
     );
 }
