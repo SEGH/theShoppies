5c149e55a9f851a9fa9932268764934a8b3eb5a5
Susan Holland <segh@fastmail.com>
2021-05-06 16:49:05 -0400

    Call handleSearch on form submit

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,9 @@
 import { TextInput } from 'react-materialize'
 
-export default function SearchBar({ handleInputChange }) {
+export default function SearchBar({ handleInputChange, handleSearch, searchValue }) {
     return (
-        <form>
-            <TextInput icon="search" label="Search" onChange={handleInputChange} />
+        <form onSubmit={handleSearch}>
+            <TextInput icon="search" label="Search" onChange={handleInputChange} value={searchValue} />
         </form>
     )
 }
