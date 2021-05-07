87803202bb5117e33b49f5231c660e60616e1587
Susan Holland <segh@fastmail.com>
2021-05-07 15:14:43 -0400

    Import stylesheet

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,8 +1,9 @@
+import './SearchBar.css';
 import { TextInput } from 'react-materialize'
 
 export default function SearchBar({ handleInputChange, handleSearch, searchValue }) {
     return (
-        <form onSubmit={handleSearch}>
+        <form id="searchBar" onSubmit={handleSearch}>
             <TextInput icon="search" label="Search" onChange={handleInputChange} value={searchValue} />
         </form>
     )
