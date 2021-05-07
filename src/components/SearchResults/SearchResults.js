b0313735fcab0fecfee9783b809641a30f9e744c
Susan Holland <segh@fastmail.com>
2021-05-07 13:23:20 -0400

    Conditionally add disabled class to button if movie imdbID matches that of one in nominations array

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,9 +1,15 @@
 import { Collection, CollectionItem, Button } from 'react-materialize';
 
-export default function SearchResults({ movieData, searchError, nominateMovie }) {
+export default function SearchResults({ movieData, searchError, nominateMovie, nominations }) {
+    console.log(movieData)
     return (
         <Collection header={searchError !== "" && searchError}>
-            {movieData.length > 0 && movieData.map((movie, index) => <CollectionItem key={index}>{movie.Title} {movie.Year}<Button onClick={() => nominateMovie(movie)}>Nominate</Button></CollectionItem>)}
+            {movieData.length > 0 && movieData.map((movie, index) => (
+                <CollectionItem key={index}>{movie.Title} {movie.Year}
+                    <Button className={nominations.filter(obj => obj.imdbID === movie.imdbID).length > 0 ? "disabled" : ""}
+                        onClick={() => nominateMovie(movie)}>Nominate</Button>
+                </CollectionItem>
+            ))}
         </Collection>
     )
 }
