13a3d9a0d83a63f31ab392e8791bbe95ccfab070
Susan Holland <segh@fastmail.com>
2021-05-07 13:30:49 -0400

    Call removeMovie function on button click

--- src/components/Nominations/Nominations.js
+++ src/components/Nominations/Nominations.js
@@ -1,9 +1,14 @@
-import { Collection, CollectionItem } from 'react-materialize';
+import { Collection, CollectionItem, Button } from 'react-materialize';
 
-export default function Nominations({ nominations }) {
+export default function Nominations({ nominations, removeMovie }) {
     return (
         <Collection>
-            {nominations.length > 0 && nominations.map((movie, index) => <CollectionItem key={index}>{movie.Title}</CollectionItem>)}
+            {nominations.length > 0 && nominations.map((movie, index) => (
+                <CollectionItem key={index}>
+                    {movie.Title} {movie.Year}
+                    <Button onClick={() => removeMovie(movie)}>Remove</Button>
+                </CollectionItem>
+            ))}
         </Collection>
     )
 }
