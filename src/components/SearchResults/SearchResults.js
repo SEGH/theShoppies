87eff4f79dde9313d158e1ffc17c7a835b184051
Susan Holland <segh@fastmail.com>
2021-05-07 14:53:43 -0400

    Update header to conditionally render lastSearched results or error

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,10 +1,10 @@
 import './SearchResults.css';
 import { Collection, CollectionItem, Button } from 'react-materialize';
 
-export default function SearchResults({ movieData, searchError, nominateMovie, nominations }) {
+export default function SearchResults({ movieData, lastSearched, searchError, nominateMovie, nominations }) {
     console.log(movieData)
     return (
-        <Collection id="searchResults" header={searchError !== "" && searchError}>
+        <Collection id="searchResults" header={searchError !== "" ? searchError : movieData.length > 0? `Results for "${lastSearched}"` : ""}>
             {movieData.length > 0 && movieData.map((movie, index) => (
                 <CollectionItem key={index}>{movie.Title} {movie.Year}
                     <Button className={nominations.filter(obj => obj.imdbID === movie.imdbID).length > 0 ? "disabled" : ""}
