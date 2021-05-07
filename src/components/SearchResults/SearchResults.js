7ab2b1b39e882ae8bd0ded550f78a3cc353830cf
Susan Holland <segh@fastmail.com>
2021-05-06 21:17:08 -0400

    Add onClick to Buttons to call nominateMovie with movie as argument

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,9 +1,9 @@
 import { Collection, CollectionItem, Button } from 'react-materialize';
 
-export default function SearchResults({ movieData, searchError }) {
+export default function SearchResults({ movieData, searchError, nominateMovie }) {
     return (
         <Collection header={searchError !== "" && searchError}>
-            {movieData.length > 0 && movieData.map((movie, index) => <CollectionItem key={index}>{movie.Title} {movie.Year}<Button>Nominate</Button></CollectionItem>)}
+            {movieData.length > 0 && movieData.map((movie, index) => <CollectionItem key={index}>{movie.Title} {movie.Year}<Button onClick={() => nominateMovie(movie)}>Nominate</Button></CollectionItem>)}
         </Collection>
     )
 }
