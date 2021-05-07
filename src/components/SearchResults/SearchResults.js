b9edf61b55725e555e5d584ba5e2be5d0c9671df
Susan Holland <segh@fastmail.com>
2021-05-07 14:45:26 -0400

    Import stylesheet, add searchResults id

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,9 +1,10 @@
+import './SearchResults.css';
 import { Collection, CollectionItem, Button } from 'react-materialize';
 
 export default function SearchResults({ movieData, searchError, nominateMovie, nominations }) {
     console.log(movieData)
     return (
-        <Collection header={searchError !== "" && searchError}>
+        <Collection id="searchResults" header={searchError !== "" && searchError}>
             {movieData.length > 0 && movieData.map((movie, index) => (
                 <CollectionItem key={index}>{movie.Title} {movie.Year}
                     <Button className={nominations.filter(obj => obj.imdbID === movie.imdbID).length > 0 ? "disabled" : ""}
