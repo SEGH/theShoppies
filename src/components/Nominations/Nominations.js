f59af6d31dcb1cdf05833f1b3770b61cc39178f2
Susan Holland <segh@fastmail.com>
2021-05-07 15:21:07 -0400

    Group main.Title and main.year in span, child of CollectionItem

--- src/components/Nominations/Nominations.js
+++ src/components/Nominations/Nominations.js
@@ -1,11 +1,12 @@
+import './Nominations.css';
 import { Collection, CollectionItem, Button } from 'react-materialize';
 
 export default function Nominations({ nominations, removeMovie }) {
     return (
-        <Collection>
+        <Collection id="nominations">
             {nominations.length > 0 && nominations.map((movie, index) => (
                 <CollectionItem key={index}>
-                    {movie.Title} {movie.Year}
+                    <span>{movie.Title} {movie.Year}</span>
                     <Button onClick={() => removeMovie(movie)}>Remove</Button>
                 </CollectionItem>
             ))}
