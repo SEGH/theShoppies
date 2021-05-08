1a1c3a851241db670eda0ca2bdcd89c3b107a54a
Susan Holland <segh@fastmail.com>
2021-05-08 11:28:51 -0400

    Render h6 if message

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,10 @@
 import './Banner.css';
 
-export default function Banner({ completed }) {
+export default function Banner({ completed, message }) {
     return (
         <section id="banner" className={completed < 5 ? "hide" : ""}>
             <h2>Nominations Complete!</h2>
+            {message && <h6>{message}</h6>}
         </section>
     )
 }
