8c67883d91e0d4a3354fdc4e3051175c066f7ae7
Susan Holland <segh@fastmail.com>
2021-05-08 09:58:05 -0400

    Display icon when box is empty

--- src/components/Nominations/Nominations.js
+++ src/components/Nominations/Nominations.js
@@ -1,15 +1,17 @@
 import './Nominations.css';
-import { Collection, CollectionItem, Button } from 'react-materialize';
+import { Collection, CollectionItem, Button, Icon } from 'react-materialize';
 
 export default function Nominations({ nominations, removeMovie }) {
     return (
         <Collection id="nominations" header={nominations.length === 0 ? 'No Nominations' : 'Nominations'}>
-            {nominations.length > 0 && nominations.map((movie, index) => (
+            {nominations.length > 0 ? nominations.map((movie, index) => (
                 <CollectionItem key={index}>
                     <span>{movie.Title} {movie.Year}</span>
                     <Button onClick={() => removeMovie(movie)}>Remove</Button>
                 </CollectionItem>
-            ))}
+            ))
+            : <CollectionItem className="noMoviesIcon"><Icon large center>movies</Icon></CollectionItem>
+        }
         </Collection>
     )
 }
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,17 +1,19 @@
 import './SearchResults.css';
-import { Collection, CollectionItem, Button } from 'react-materialize';
+import { Collection, CollectionItem, Button, Icon } from 'react-materialize';
 
 export default function SearchResults({ movieData, lastSearched, searchError, nominateMovie, nominations }) {
     console.log(movieData)
     return (
-        <Collection id="searchResults" header={searchError !== "" ? searchError : movieData.length > 0? `Results for "${lastSearched}"` : ""}>
-            {movieData.length > 0 && movieData.map((movie, index) => (
+        <Collection id="searchResults" header={searchError !== "" ? searchError : movieData.length > 0 ? `Results for "${lastSearched}"` : 'Results'}>
+            {movieData.length > 0 ? movieData.map((movie, index) => (
                 <CollectionItem key={index}>
                     <span>{movie.Title} ({movie.Year})</span>
                     <Button small className={nominations.filter(obj => obj.imdbID === movie.imdbID).length > 0 ? "disabled" : ""}
                         onClick={() => nominateMovie(movie)}>Nominate</Button>
                 </CollectionItem>
-            ))}
+            ))
+                : <CollectionItem className="noMoviesIcon"><Icon large center>movies</Icon></CollectionItem>
+            }
         </Collection>
     )
 }
