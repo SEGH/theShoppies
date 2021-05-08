67acfd6517583c96d5611b5473a64e905af7599a
Susan Holland <segh@fastmail.com>
2021-05-08 11:49:07 -0400

    Change fetch url to https

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 const API = {
     getMovies: async function(query) {
-        const response = await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}&s=${query}&type=movie`);
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}&s=${query}&type=movie`);
         const movies = await response.json();
         return movies;
     }
